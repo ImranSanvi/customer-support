b63f5ccd4c7983978d4db55fcb9d445f8cf1655d
ImranSanvi <imranmohammed5220@gmail.com>
2025-10-18 23:11:16 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+
+import './App.css'
+  import { ToastContainer} from 'react-toastify';
+
+function App() {
+  // const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div className='mx-[280px]'>
+        
+      </div>
+
+      <ToastContainer />
+    </>
+  )
+}
+
+export default App
